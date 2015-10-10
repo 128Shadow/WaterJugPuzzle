c50bf2d3b7d81bacb34534267da4933bcd288d4b
imBulbi <mlggrizz3@gmail.com>
2015-10-09 21:35:40 -0400

    Main function changed

--- WaterJug/waterjug.cpp
+++ WaterJug/waterjug.cpp
@@ -23,7 +23,8 @@ public:
 			distribution_(new int[3]), first_(a), second_(b), third_(c) {
 		set();
 	}
-	~Jugs() {
+	~Jugs() 
+	{
 		delete[] distribution_;
 	}
 	inline int* distribution() const {
@@ -49,19 +50,152 @@ private:
 	}
 };
 */
-void BFS(vector<int> &curr, vector<int> &target, vector< vector<int> > &visited, queue< vector<int> > &search) {
+
+/*
+void BFS(vector<int> &curr, vector<int> &target, vector< vector<int> > &visited, queue< vector<int> > &search) 
+{
 	cout << curr[2] << endl;
 	//base case curr == target
+	if (!(visited[curr[0], curr[1]]))
+	{
+		visited[curr[0], curr[1]] = true;
+		
+	}
+		
 	//check if curr is visited
 		//if yes end
 		//if no
 			//recursively call
 }
-int main(int argc, char * const argv[]) {
+*/
+
+bool parseArg(vector<int> &jarSize, vector<int> &target)
+{
+	for (int i = 0; i < 3; ++i)
+	{
+		if (target[i] > jarSize[i])
+		{
+			cerr << "Goal cannot exceed capacity of jug" << endl;
+			return false;
+		}
+	}
+}
+
+int main(int argc, char * const argv[])
+{
 	//array[ array[bool] ] visited
 	/*Jugs *test = new Jugs(1, 3, 5);
 	cout << test->first() << endl;
 	*/
+	if (argc != 7)
+	{
+		cerr << "Usage: ./waterjugpuzzle <cap A> <cap B> <cap C> <goal A> <goal B> <goal C>" << endl;
+		return 1;
+	}
+	//Create two vectors initial and target in main function
+	istringstream iss;
+	vector<int> jarSizes(3);
+	vector<int> target(3);
+	int val;
+	string str;
+	
+	for (int i = 1; i < argc; ++i)
+	{
+		iss.str(argv[i]);
+		
+		switch (i)
+		{
+			case (1):
+				if ((!(iss >> val)) || (val <= 0))
+				{
+					cerr << "Invalid capacity '" << argv[i] << "' for jug A." << endl;
+					return 1;
+				}
+				jarSizes[0] = val;
+				break;
+			case (2):
+				if ((!(iss >> val)) || (val <= 0))
+				{
+					cerr << "Invalid capacity '" << argv[i] << "' for jug B." << endl;
+					return 1;
+				}
+				jarSizes[1] = val;
+				break;
+			case (3):
+				if ((!(iss >> val)) || (val <= 0))
+				{
+					cerr << "Invalid capacity '" << argv[i] << "' for jug C." << endl;
+					return 1;
+				}
+				jarSizes[2] = val;
+				break;
+			case (4):
+				if ((!(iss >> val)) || (val < 0))
+				{
+					cerr << "Invalid goal '" << argv[i] << "' for jug A." << endl;
+					return 1;
+				}
+				target[0] = val;
+				break;
+			case (5):
+				if ((!(iss >> val)) || (val < 0))
+				{
+					cerr << "Invalid goal '" << argv[i] << "' for jug B." << endl;
+					return 1;
+				}
+				target[1] = val;
+				break;
+			case (6):
+				if ((!(iss >> val)) || (val < 0))
+				{
+					cerr << "Invalid goal '" << argv[i] << "' for jug C." << endl;
+					return 1;
+				}
+				target[2] = val;
+				break;
+		}
+		iss.clear();
+	}
+	//In parseArg, have two parameters that take vector<int>
+	parseArg(jarSizes, target);
+	/*
+	istringstream iss;
+	int val;
+	string str;
+	for (int i = 1; i < argc; ++i)
+	{
+		iss.str(argv[i]);
+		iss >> str;
+		if (i <= 3)
+		{
+			if ((!(iss >> val))|| (val <= 0))
+			{
+				switch (i)
+				{
+					case (1):
+						cerr << "Invalid goal '" << str << "' for jug A." << endl;
+						return 1;
+					case (2):
+						cerr << "Invalid goal '" << str << "' for jug B." << endl;
+						return 1;
+					case (3):
+						cerr << "Invalid goal '" << str << "' for jug C." << endl;
+						return 1;
+				}
+			}
+		}
+		else
+		{
+			if (!(iss >> val) && (val < 0))
+			{
+				cerr << "Invalid capacity for " << endl;
+				return 1;
+			}
+		}
+		iss.clear();
+	}
+	*/
+		/*
 	vector<int> test;
 	test.push_back(0);
 	test.push_back(0);
@@ -70,9 +204,11 @@ int main(int argc, char * const argv[]) {
 	target.push_back(3);
 	target.push_back(5);
 	target.push_back(0);
-	vector< vector<int> > visited;
+	vector< vector<bool> > visited;
+	
 	queue< vector<int> > search;
-	BFS(test, target,visited, search);
+	BFS(test, target, visited, search);
+	*/
 	return 0;
 }
 
